6521e3cee218b6f91b2a60dc1080dd5efdba8b8d
thiagodinizdev@yahoo.com.br <8269Jolie>
2017-07-24 11:46:09 -0300

    Correção do migrate para o modelo user

--- db/migrate/20170724122304_devise_token_auth_create_users.rb
+++ db/migrate/20170724122304_devise_token_auth_create_users.rb
@@ -16,11 +16,11 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.1]
       t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, :default => 0, :null => false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      t.integer  :sign_in_count, :default => 0, :null => false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
@@ -48,7 +48,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.1]
     add_index :users, :email,                unique: true
     add_index :users, [:uid, :provider],     unique: true
     add_index :users, :reset_password_token, unique: true
-    add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,       unique: true
   end
 end
